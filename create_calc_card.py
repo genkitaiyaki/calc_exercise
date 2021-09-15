179ec4776d07413fec7ab08b3cc3901bcb147b8e
genkitaiyaki <genkitaiyaki@gmail.com>
2021-09-16 08:10:22 +0900

    refactor:  contoroller内の処理をserviceクラスに移動
     contoroller内の処理が肥大化気味だったので処理を移動した
    今後別のserviceを追加しそうな気がするのでクラスの命名はcommon_serviceにした

--- create_calc_card.py
+++ create_calc_card.py
@@ -1,94 +1,10 @@
-import random   
-from models.formula import Formula
+from services.common_service import CommonService
 from models.exercise_parts import ExerciseParts
-import jinja2
-import numpy as np
-from itertools import product
-from typing import Callable
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-import json
-from selenium.webdriver.support import expected_conditions as EC
-import os
 
-# TODO: 肥大化気味なのでServiceクラスを作り移動する
 # TODO: docstringをちゃんと書いて不要なコメントを消す
 # TODO: 今後のことを考えコントローラークラスにする
 
-def create_formulas(
-    exercise_parts: ExerciseParts,
-    validater: Callable,
-    needs_shuffle: bool = True
-    ) ->list[Formula]:
-    """
-    計算式リストを作成する
-    """
-
-    formulas: list[str] = []
-    for left_num, right_num in product(exercise_parts.lefts, exercise_parts.rights):
-        if (not validater(left_num, right_num)): continue
-        formula = Formula(left_num, right_num, exercise_parts.operater).to_string()
-        formulas.append(formula)
-
-    if (needs_shuffle): random.shuffle(formulas)
-    return formulas
-
-# 計算リストをPDFで出力する関数
-def export_exercise(
-    title: str,
-    formulas: list[str]
-    ) -> None:
-
-    # Chrome の印刷機能でPDFとして保存
-    # pdfkitだと表示崩れがおきるためseleniumで印刷処理を行う
-    options = webdriver.ChromeOptions()
-    # PDF印刷設定
-    appState = {
-        "recentDestinations": [
-            {
-                "id": "Save as PDF",
-                "origin": "local",
-                "account": ""
-            }
-        ],
-        "selectedDestinationId": "Save as PDF",
-        "version": 2,
-        "pageSize": 'A4'
-    }
-    # ドライバへのPDF印刷設定の適用
-    # TODO: 任意のフォルダに出力できるようにする
-    dir = os.path.dirname(os.path.abspath('__file__'))
-    options.add_experimental_option("prefs", {
-        "printing.print_preview_sticky_settings.appState": json.dumps(appState),
-        "safebrowsing.enabled": True,
-    })
-    options.add_argument('--kiosk-printing')
-    options.add_argument('--headless')
-
-    # jinja2でtemplateを読み込む
-    # TODO: template.html周りははModel化する？
-    fsloader = jinja2.FileSystemLoader(searchpath='./templates')
-    env = jinja2.Environment(loader=fsloader)
-    template = env.get_template('template.html')
-    MAX_COL = 4 # A4印刷だと4列がいい感じになる
-    html = template.render({
-	    'title': title
-	    ,'MAX_COL': MAX_COL
-	    ,'formula_columns': np.array_split(formulas, MAX_COL) 
-    })
-
-    # TODO: chromedriverの存在チェック処理を入れる
-
-    with webdriver.Chrome("./chromedriver", options=options) as driver:
-        wait = WebDriverWait(driver, 15)
-        driver.implicitly_wait(10)
-        
-        driver.get("data:text/html;charset=utf-8," + html)
-        # ページ上のすべての要素が読み込まれるまで待機
-        wait.until(EC.presence_of_all_elements_located)
-        # PDFとして印刷
-        driver.execute_script('window.print()')
-        driver.quit()
+common_service = CommonService()
 
 # 足し算式を作る
 def add_validater(left_num: int, right_num: int) -> bool:
@@ -99,7 +15,7 @@ def add_validater(left_num: int, right_num: int) -> bool:
     return True
 
 add_exercise_parts = ExerciseParts(list(range(0,10)), list(range(0,10)), "+")
-add_formulas = create_formulas(add_exercise_parts, add_validater)
+add_formulas = common_service.create_formulas(add_exercise_parts, add_validater)
 
 # 引き算式を作る
 def sub_validater(left_num: int, right_num: int) -> bool:
@@ -109,8 +25,8 @@ def sub_validater(left_num: int, right_num: int) -> bool:
     if(right_num == 0): return False
     return True
 sub_exercises = ExerciseParts(list(range(0,11)), list(range(0,11)), "-")
-sub_formulas = create_formulas(sub_exercises, sub_validater)
+sub_formulas = common_service.create_formulas(sub_exercises, sub_validater)
 
 # 出力
-export_exercise('たしざんのもんだい', add_formulas)
-export_exercise('ひきざんのもんだい', sub_formulas)
+common_service.export_exercise('たしざんのもんだい', add_formulas)
+common_service.export_exercise('ひきざんのもんだい', sub_formulas)
---
+++ services/common_service.py
@@ -0,0 +1,89 @@
+from typing import Callable
+from models.formula import Formula
+from models.exercise_parts import ExerciseParts
+from itertools import product
+from typing import Callable
+import random
+import jinja2
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import json
+from selenium.webdriver.support import expected_conditions as EC
+import os
+
+class CommonService():
+    def create_formulas(
+        exercise_parts: ExerciseParts,
+        validater: Callable,
+        needs_shuffle: bool = True
+        ) ->list[Formula]:
+        """
+        計算式リストを作成する
+        """
+    
+        formulas: list[str] = []
+        for left_num, right_num in product(exercise_parts.lefts, exercise_parts.rights):
+            if (not validater(left_num, right_num)): continue
+            formula = Formula(left_num, right_num, exercise_parts.operater).to_string()
+            formulas.append(formula)
+    
+        if (needs_shuffle): random.shuffle(formulas)
+        return formulas
+
+    # 計算リストをPDFで出力する関数
+    def export_exercise(
+        title: str,
+        formulas: list[str]
+        ) -> None:
+
+        # Chrome の印刷機能でPDFとして保存
+        # pdfkitだと表示崩れがおきるためseleniumで印刷処理を行う
+        options = webdriver.ChromeOptions()
+        # PDF印刷設定
+        appState = {
+            "recentDestinations": [
+                {
+                    "id": "Save as PDF",
+                    "origin": "local",
+                    "account": ""
+                }
+            ],
+            "selectedDestinationId": "Save as PDF",
+            "version": 2,
+            "pageSize": 'A4'
+        }
+        # ドライバへのPDF印刷設定の適用
+        # TODO: 任意のフォルダに出力できるようにする
+        dir = os.path.dirname(os.path.abspath('__file__'))
+        options.add_experimental_option("prefs", {
+            "printing.print_preview_sticky_settings.appState": json.dumps(appState),
+            "safebrowsing.enabled": True,
+        })
+        options.add_argument('--kiosk-printing')
+        options.add_argument('--headless')
+
+        # jinja2でtemplateを読み込む
+        # TODO: template.html周りははModel化する？
+        fsloader = jinja2.FileSystemLoader(searchpath='./templates')
+        env = jinja2.Environment(loader=fsloader)
+        template = env.get_template('template.html')
+        MAX_COL = 4 # A4印刷だと4列がいい感じになる
+        html = template.render({
+    	    'title': title
+    	    ,'MAX_COL': MAX_COL
+    	    ,'formula_columns': np.array_split(formulas, MAX_COL) 
+        })
+
+        # TODO: chromedriverの存在チェック処理を入れる
+
+        with webdriver.Chrome("./chromedriver", options=options) as driver:
+            wait = WebDriverWait(driver, 15)
+            driver.implicitly_wait(10)
+
+            driver.get("data:text/html;charset=utf-8," + html)
+            # ページ上のすべての要素が読み込まれるまで待機
+            wait.until(EC.presence_of_all_elements_located)
+            # PDFとして印刷
+            driver.execute_script('window.print()')
+            driver.quit()
