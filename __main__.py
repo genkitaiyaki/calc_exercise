c86c2dc6063d4c96b5299bab993ace4e0208f44b
genkitaiyaki <genkitaiyaki@gmail.com>
2021-09-23 21:43:17 +0900

    feat: 計算種類選択のCLI実装
    好きな計算問題を出力できるよう、計算の種類を選択できるCLIを実装した

--- __main__.py
+++ __main__.py
@@ -1,8 +1,32 @@
+from __future__ import print_function, unicode_literals
+
 from models.formula import Formula
 from services.common_service import CommonService
 from services.base_calc_service import BaseCalcService
 from services.single_digi_add_service import SingleDigiAddService
 from services.single_digi_sub_service import SingleDigiSubService
+from pyfiglet import figlet_format
+from PyInquirer import prompt
+
+print(figlet_format('CALC EXERCISE'))
+
+questions = [
+    {
+        'type': 'checkbox',
+        'message': 'Select calc type.',
+        'name': 'calc_types',
+        'choices': [
+            {
+                'name': 'add (+)'
+            },
+            {
+                'name': 'sub (-)'
+            },
+        ]
+     }
+]
+answers = prompt(questions)
+calc_types: list[str] = answers.get('calc_types')
 
 # TODO: もっと良い命名がありそう、、
 def execute(calc_service: BaseCalcService, title: str):
@@ -13,5 +37,5 @@ def execute(calc_service: BaseCalcService, title: str):
     )
     common_service.export_exercise(title, formulas)
 
-execute(SingleDigiAddService(), 'たしざんのもんだい')
-execute(SingleDigiSubService(), 'ひきざんのもんだい')
+if ('add (+)' in calc_types) : execute(SingleDigiAddService(), 'たしざんのもんだい')  
+if ('sub (-)' in calc_types) : execute(SingleDigiSubService(), 'ひきざんのもんだい')
